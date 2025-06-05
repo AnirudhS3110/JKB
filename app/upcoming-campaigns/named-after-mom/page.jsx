5157d43f56c31210b9762d0a516c1b073f8a5acf
AnirudhS3110 <anirudh23bcs19@iiitkottayam.ac.in>
2025-06-05 14:03:40 +0530

    added desc to named after mom

--- app/upcoming-campaigns/named-after-mom/page.jsx
+++ app/upcoming-campaigns/named-after-mom/page.jsx
@@ -4,9 +4,9 @@ import CampaignPage from '../../../components/ui/CampaignPage';
 export default function EternalFlameCampaignPage() {
   return (
     <CampaignPage 
-      title="Eternal Flame Campaign"
-      subtitle="Honouring National Icons, Preserving Legacies"
-      description="This national recognition campaign seeks to immortalize great personalities through postal stamps, commemorative coins, public spaces, and published biographies—ensuring their contributions continue to inspire future generations."
+      title="Named After Mom"
+      subtitle="Take Her Name. Carry Her Legacy."
+      description="Named After Mom is a campaign to include your mother’s name as your middle name in all official documents. It challenges the default use of the father’s name and honors the woman who raised, nurtured, and shaped your identity."
       heroImage="https://images.unsplash.com/photo-1531379410502-63bfe8cdaf6f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
       campaignImage="/images/NamedAfterMom.jpg"
     />
